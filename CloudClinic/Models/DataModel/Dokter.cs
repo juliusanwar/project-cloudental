22c3fe70d8181935bef78c633eb5eeba44140549
Erick Kurniawan <erick.kurniawan@gmail.com>
2015-03-06 11:14:48 +0700

    Menambahkan model Dokter

---
+++ CloudClinic/Models/DataModel/Dokter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CloudClinic.Models
+{
+    public class Dokter
+    {
+        public int IdDokter { get; set; }
+        public string Nama { get; set; }
+        public string Alamat { get; set; }
+        public string Telp { get; set; }
+    }
+}
