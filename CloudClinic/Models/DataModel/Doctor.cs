93522a71e8370baa9b7366f96942a300756c1bee
Erick Kurniawan <erick.kurniawan@gmail.com>
2015-03-06 12:01:14 +0700

    menambahkan model patient

--- CloudClinic/Models/DataModel/Doctor.cs
+++ CloudClinic/Models/DataModel/Doctor.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace CloudClinic.Models
 {
-    public class Dokter
+    public class Doctor
     {
-        public int IdDokter { get; set; }
+        [Key]
+        public string DoctorId { get; set; }
+        [Index(IsUnique = true)]
+        public string UserName { get; set; }
         public string Nama { get; set; }
         public string Alamat { get; set; }
         public string Telp { get; set; }
