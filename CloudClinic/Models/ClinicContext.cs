d619b905d95eff6ab0779d188f86cf476ec57dde
juliusanwar <julius.anwar@si.ukdw.ac.id>
2015-03-12 15:37:07 +0700

    Menambahkan Model ClinicContext

---
+++ CloudClinic/Models/ClinicContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace CloudClinic.Models
+{
+    public class ClinicContext : DbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Obat> Obats { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<RekamMedis> RekamMedisis { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Tindakan> Tindakans { get; set; }
+        public DbSet<JenisTindakan> JenisTindakans { get; set; }
+        public DbSet<BillingJasa> BillingJasas { get; set; }
+        public DbSet<BillingObat> BillingObats { get; set; }
+    }
+}
