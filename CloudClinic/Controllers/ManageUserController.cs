b3d459ca54b3d23593da5e1ae804021fae9ed8a3
Erick Kurniawan <erick.kurniawan@gmail.com>
2015-04-08 08:13:05 +0700

    fitur delete role

--- CloudClinic/Controllers/ManageUserController.cs
+++ CloudClinic/Controllers/ManageUserController.cs
@@ -49,17 +49,26 @@ namespace CloudClinic.Controllers
             return View();
         }
 
-        public ActionResult ShowAllRole()
+        private IEnumerable<RoleViewModel> GetAllRoles()
         {
             var roles = RoleManager.Roles;
             List<RoleViewModel> model = new List<RoleViewModel>();
 
-            foreach(var role in roles)
+            foreach (var role in roles)
             {
                 model.Add(new RoleViewModel { RoleId = role.Id, RoleName = role.Name });
             }
 
+            return model;
+        }
+
+        public ActionResult ShowAllRole()
+        {
+            var model = GetAllRoles();
+
             return View(model);
+
+            
         }
 
         public ActionResult AddRole()
@@ -81,22 +90,89 @@ namespace CloudClinic.Controllers
                     var result = RoleManager.Create(newRole);
                     if (result.Succeeded)
                     {
+                        msgbox.TitleMessage = "Info";
                         msgbox.AlertClass = "alert alert-info";
                         msgbox.MessageBody = "Data Role " + role.RoleName + " berhasil ditambah !";
                     }
                     else
                     {
-                        msgbox.AlertClass = "alert alert-warning";
+                        msgbox.TitleMessage = "Error!";
+                        msgbox.AlertClass = "alert alert-error";
                         msgbox.MessageBody = "Gagal menambahkan role !";
                     } 
                 }
                 else
                 {
+                    msgbox.TitleMessage = "Warning!";
                     msgbox.AlertClass = "alert alert-warning";
-                    msgbox.MessageBody = "Role yang anda inputkan sudah ada, masukan yang lain !";
+                    msgbox.MessageBody = "Role "+ role.RoleName +" sudah ada, masukan yang lain !";
                 }
             }
             return PartialView("_MessageBox", msgbox);
+
+            
+        }
+
+        public ActionResult UpdateRole(string id)
+        {
+            RoleViewModel model = new RoleViewModel();
+            var role = RoleManager.FindById(id);
+            if(role!=null)
+            {
+                model.RoleId = role.Id;
+                model.RoleName = role.Name;
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("ShowAllRole");
+            }
+            
+        }
+
+        [HttpPost]
+        public ActionResult UpdateRole(RoleViewModel model)
+        {
+            MessageBoxViewModel msgbox = new MessageBoxViewModel();
+
+            if (ModelState.IsValid)
+            {
+                var role = RoleManager.FindById(model.RoleId);
+                if (role != null)
+                {
+                    role.Name = model.RoleName;
+                    var result = RoleManager.Update(role);
+                    if (result.Succeeded)
+                    {
+                        msgbox.TitleMessage = "info";
+                        msgbox.MessageBody = "Role " + model.RoleName + " sudah berhasil diupdate !";
+                        msgbox.AlertClass = "alert alert-info";
+                    }
+                    else
+                    {
+                        msgbox.TitleMessage = "Error!";
+                        msgbox.MessageBody = "Gagal mengupdate role " + model.RoleName;
+                        msgbox.AlertClass = "alert alert-error";
+                    }
+                } 
+            }
+            return PartialView("_MessageBox", msgbox);
+        }
+
+        public ActionResult DeleteRole(string id)
+        {
+            var role = RoleManager.FindById(id);
+            if(role!=null)
+            {
+                var result = RoleManager.Delete(role);
+               
+                ViewBag.TitleMessage = "Konfirmasi";
+                ViewBag.MessageBody = "Data role " + role.Name + " berhasil di delete";
+                ViewBag.AlertClass = "alert alert-info";
+            }
+
+            IEnumerable<RoleViewModel> model = GetAllRoles();
+            return PartialView("_PartialListRole", model);
         }
 
 
--- CloudClinic/Models/ViewModel/MessageBoxViewModel.cs
+++ CloudClinic/Models/ViewModel/MessageBoxViewModel.cs
@@ -7,6 +7,7 @@ namespace CloudClinic.Models.ViewModel
 {
     public class MessageBoxViewModel
     {
+        public string TitleMessage { get; set; }
         public string MessageBody { get; set; }
         public string AlertClass { get; set; }
     }
